e6f8946bb962894e550e00654f2bd7e08e8c448a
aleenashajan2003 <119517009+aleenashajan2003@users.noreply.github.com>
2025-09-03 09:02:57 +0530

    even numbers from 1 to 20.c

---
+++ even numbers from 1 to 20.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    printf("Even numbers from 1 to 20 \n");
+    for (int i = 1; i <= 20; i++) {
+        if (i % 2 == 0) {
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
+    return 0;
+}
